4d688551868dc5447e06649e42caea5ab7be5bc8
kathyleboeuf <kleboeuf@townisp.com>
2018-09-04 17:09:16 -0400

    added dispatch action

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 
 import './feedback.css';
 
-export default function Feedback(props) {
-  /** 
-   * Below, we'll use the guessCount to generate a key so that React treats the feedback message 
+export function Feedback(props) {
+  /**
+   * Below, we'll use the guessCount to generate a key so that React treats the feedback message
    * as a DOM change, even when a guess does not change the feedback text.
    * This is necessary for consistent aural feedback via aria-live.
   */
@@ -15,7 +17,7 @@ export default function Feedback(props) {
     guessAgain = <span className="visuallyhidden">Guess again!</span>;
   }
   return (
-    <h2 
+    <h2
       key={key}
       id="feedback"
       role="status"
@@ -26,3 +28,9 @@ export default function Feedback(props) {
     </h2>
   );
 }
+const mapStateToProps = state => ({
+  guessCount: state.guesses.length,
+  feedback: state.feedback
+});
+
+export default connect(mapStateToProps)(Feedback);
--- src/components/header.js
+++ src/components/header.js
@@ -4,16 +4,10 @@ import TopNav from './top-nav';
 
 import './header.css';
 
-import { restartGame } from '../actions'; //do we need this here?
-
 export default function Header(props) {
   return (
     <header>
-      <TopNav
-        onGenerateAuralUpdate={() => props.onGenerateAuralUpdate()}
-        // onRestartGame={() => props.onRestartGame()}
-        onRestartGame={(e) => props.dispatch(restartGame(e.target.value))}
-      />
+      <TopNav />
       <h1>HOT or COLD</h1>
     </header>
   );
