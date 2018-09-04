d4322b3b90a1fe0f2292aa50941b3eba28a51a10
kathyleboeuf <kleboeuf@townisp.com>
2018-09-04 14:52:30 -0400

    added actions

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const RESTART_GAME = 'RESTART_GAME';
+export const restartGame = () => ({
+	type: RESTART_GAME
+})
+
+
+export const MAKE_GUESS = 'MAKE_GUESS';
+export const makeGuess = guess => ({
+	type: MAKE_GUESS,
+	guess
+})
